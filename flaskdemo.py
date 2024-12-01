2465d348d17ad1746271600fa6f2044faf5f1860
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-12-01 21:27:06 +1000

    Complete Wiki + Flask exercise

--- flaskdemo.py
+++ flaskdemo.py
@@ -13,7 +13,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "I am still working on this"
+    return render_template("about.html")
 
 
 @app.route('/search', methods=['POST', 'GET'])
@@ -28,7 +28,7 @@ def search():
 def results():
     search_term = session['search_term']
     page = get_page(search_term)
-    return render_template("results.html", page=page)
+    return render_template("results.html", page=page, title=page.title)
 
 
 def get_page(search_term):
